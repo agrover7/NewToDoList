718c52898df347819b863935328d96afb4af9232
Aman Grover <34801581+agrover7@users.noreply.github.com>
2018-11-12 16:43:55 -0600

    minimum completed

--- scripts.js
+++ scripts.js
@@ -1,5 +1,28 @@
 var unique_key = "80dcc13e2aed041d334bbe2410a977eeb28581b2fd73fa42bd5fef86601318c2"
 
+//To load Todos that already exist
+var listRequest = new XMLHttpRequest();
+listRequest.onreadystatechange = function(){
+  if(this.readyState ==4 && this.status == 200)
+  {
+    var todos = JSON.parse(this.responseText);
+    for(var i = 0; i < todos.length; i++)
+    {
+        renderTodo(todos[i]);
+    }
+  }
+  else if(this.readyState == 4)
+  {
+    console.log(this.responseText);
+  }
+}
+
+listRequest.open("GET", "https://api.kraigh.net/todos", true);
+listRequest.setRequestHeader("x-api-key", unique_key);
+listRequest.send()
+
+//Display these pulled values into the page
+
 
 //To add new events
 document.getElementById("new-todo-form").addEventListener("submit", function(event) {
@@ -31,6 +54,10 @@ function renderTodo(todoData)
     var todo = document.createElement("article");
     todo.setAttribute("id", todoData.id);
     todo.classList.add("todo");
+    if(todoData.completed == true)
+    {
+      todo.classList.add("completed");
+    }
 
     var completeButton = document.createElement("button");
     completeButton.classList.add("check");
@@ -48,14 +75,47 @@ function renderTodo(todoData)
     document.getElementById("todos").appendChild(todo);
     completeButton.addEventListener("click", completeTodo);
     deleteButton.addEventListener("click", deleteTodo);
-    
+
     document.getElementById("newTitle").value = "";
 }
 
 function completeTodo(event){
-  console.log(event);
+    var todoId = event.target.parentNode.id; //event is passed to this function. target is element event is triggered for (the button). The parentNode grabs the entire article (rather than only the button). Then, we access the id.
+    var data = {
+      completed: true
+    };
+    var completeRequest = new XMLHttpRequest();
+    completeRequest.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200)
+    {
+      event.target.parentNode.classList.add("completed") //selects todo.completed
+    }
+    else if(this.readyState == 4)
+    {
+      console.log(this.response);
+    }
+}
+    completeRequest.open("PUT", "https://api.kraigh.net/todos/" + todoId, true);
+    completeRequest.setRequestHeader("Content-type", "application/json");
+    completeRequest.setRequestHeader("x-api-key", unique_key);
+    completeRequest.send(JSON.stringify(data));
 }
 
 function deleteTodo(event){
-  console.log(event);
+  var todoId = event.target.parentNode.id; //event is passed to this function. target is element event is triggered for (the button). The parentNode grabs the entire article (rather than only the button). Then, we access the id.
+  var deleteRequest = new XMLHttpRequest();
+  deleteRequest.onreadystatechange = function(){
+  if(this.readyState == 4 && this.status == 200)
+  {
+      event.target.parentNode.remove();
+  }
+  else if(this.readyState == 4)
+  {
+    console.log(this.response);
+  }
+}
+  deleteRequest.open("DELETE", "https://api.kraigh.net/todos/" + todoId, true);
+  deleteRequest.setRequestHeader("Content-type", "application/json");
+  deleteRequest.setRequestHeader("x-api-key", unique_key);
+  deleteRequest.send();
 }
